49304d11a3ea5f0780a6f78ea6a208abbd5407c5
rachelxxxxxxx <39612505+rachelxxxxxxx@users.noreply.github.com>
2021-01-06 15:43:49 +0800

    Update LSOBIA.remote.cmake

--- LSOBIA.remote.cmake
+++ LSOBIA.remote.cmake
@@ -1,6 +1,6 @@
 
 otb_fetch_module(LSOBIA
         "Large scale object based image analysis module"
-        GIT_REPOSITORY https://github.com/RTOBIA/LSOBIA.git
+        GIT_REPOSITORY https://github.com/rachelxxxxxxx/LSOBIA.git
         GIT_TAG master
         )
